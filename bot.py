2342a7ec822fc86592da0ff982a0465b9b14fdb5
Jill Enke <jill@rasias.org>
2019-08-07 11:04:02 +0200

    add bot.py

---
+++ bot.py
@@ -0,0 +1,127 @@
+# Work with Python 3.6
+import discord
+import random
+import subprocess
+import os
+import sys
+
+CMDCHAR = '!'
+DATA = '/data'
+
+TOKEN = sys.argv[1]
+client = discord.Client()
+
+def represents_int(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+def check_permissions(message):
+    if message.author.permissions_in(message.channel).kick_members == True:
+        return True
+    return False
+
+def get_server_id(message):
+    return str(message.guild.id)
+
+def get_path(name, message):
+    path = DATA + '/' + get_server_id(message) + '_' + name + '.txt'
+    return path
+
+def get_quotes(message):
+    if not os.path.exists(get_path("quotes", message)):
+        return list()
+    f = open(get_path("quotes", message), "r+")
+    f_content = f.read()
+    quotes = f_content.split('[quote]')
+    return [x.strip() for x in quotes]
+
+
+def random_quote(message):
+    quotes = get_quotes(message)
+    if len(quotes) == 0:
+        return "There are no quotes saved yet :("
+    return quotes[random.randrange(0, len(quotes),1)]
+
+
+def add_quote(quote_list, message):
+    if not check_permissions(message):
+        return "You do not have permissions to add quotes."
+    f = open(get_path("quotes", message),'a+')
+    quote = ""
+    for s in quote_list:
+        quote += s + ' '
+    f.write("[quote] " + quote.strip())
+    return "Added quote:\n" + quote
+
+
+def quotes(args, message):
+    if len(args) == 0:
+        return random_quote(message)
+    if args[0] == "path":
+        return get_path('quotes', message)
+    if args[0] == "debug":
+        return get_server_id(message)
+    if args[0] == "add":
+        if len(args) < 2:
+            return "Error: Not enough arguments."
+        return add_quote(args[1:], message)
+    if args[0] == "all":
+        quotes = get_quotes(message)
+        msg = ""
+        for idx, q in enumerate(quotes):
+            msg += str(idx + 1) + ': ' + q + '\n\n'
+        return msg.strip()
+    if represents_int(args[0]):
+        quotes = get_quotes(message)
+        qid = int(args[0])
+        if qid > len(quotes) or qid == 0:
+            return "Error: Not a valid quote #id"
+        return quotes[qid - 1]
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if not message.content.startswith(CMDCHAR):
+        return
+
+    input_list = message.content.split(' ')
+    command = input_list[0]
+    command = command[1:]
+
+    args = list()
+    
+    if len(input_list) > 1:
+        args = input_list[1:]
+
+    if command == "ping":
+        msg = 'Pong! {0.author.mention}'
+    
+    if command == "zitat" or command == "quote":
+        msg = quotes(args, message)
+
+    if command == "perm":
+        if len(args) != 2:
+            msg = "Error: Unexpected argument count."
+        if len(args) == 2 and args[0] == "add":
+            add_permissions(args[1], message)
+            msg = "Added " + args[1] + " to permissions."
+
+    msg = msg.format(message)
+
+    await message.channel.send(msg) 
+    
+@client.event
+async def on_ready():
+    if os.path.exists("/data"):
+        print('data already exists')
+    else:
+        subprocess.run(['bash', 'create_env.sh'])
+        print('data created')
+
+client.run(TOKEN)
