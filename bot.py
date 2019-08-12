9a88385d648a3bf61aa407d36f0b1c65fa6c9e9c
Jill Enke <jill.enke@gmail.com>
2019-08-12 09:18:45 +0200

    remove git.py and move it's contents to dev.py #1

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-import os
 import sys
 sys.path.insert(0, './pixie')
 import pixie.core as core
--- pixie/dev.py
+++ pixie/dev.py
@@ -1,6 +1,8 @@
-from .git import *
+from github import Github
+from github import GithubObject
 from . import messages
 from .messages import MessageCode
+from . import data
 
 
 def dev_help(message):
@@ -9,6 +11,9 @@ def dev_help(message):
 
 def dev_issue(message, split, label):
     content = message.content.split(split, 1)[1].strip()
+
+    description = ''
+
     if content[0] == '\"':
         splits = content[1:].split('\"', 1)
     else:
@@ -17,7 +22,6 @@ def dev_issue(message, split, label):
     name = splits[0]
     if len(splits) > 1:
         description = splits[1]
-
 
     description = '[Issue created by {user}]\n'.format(user=message.author.name) + description
     make_github_issue(name, description, [label, 'status: pending'])
@@ -36,3 +40,38 @@ def cmd_dev(message, args):
     if args[0] == 'bugreport':
         return dev_issue(message, args[0], 'type: bug')
     return MessageCode.UNKNOWN_ARGS
+
+
+def label_exists(repo, label):
+    labels = repo.get_labels()
+    for l in labels:
+        if label == l.name:
+            return True
+    return False
+
+
+def make_github_issue(title, body, labels=None):
+    token = open(data.DATAPATH + 'tokens/' + 'github-token', 'r').read().strip()
+    g = Github(token)
+    for repo in g.get_user().get_repos():
+        print(repo.name)
+    repo = g.get_repo('%s/%s' % (data.REPO_OWNER, data.REPO_NAME))
+
+    label_list = list()
+
+    if isinstance(labels, str):
+        if label_exists(repo, labels):
+            label_list.append(repo.get_label(labels))
+    elif labels is not None:
+        for l in labels:
+            if label_exists(repo, l):
+                label_list.append(repo.get_label(l))
+
+    if body is None:
+        body = GithubObject.NotSet
+
+    if len(label_list) == 0:
+        label_list = GithubObject.NotSet
+
+    repo.create_issue(title, body=body, labels=label_list)
+    return
