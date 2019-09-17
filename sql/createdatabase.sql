399f7653cd8065f2c3898b8f299dacb8247bf419
Darkseth93 <54412404+Darkseth93@users.noreply.github.com>
2019-09-17 14:20:43 +0200

    tpyos
    corrected some typos and added default values for timestamp

--- sql/createdatabase.sql
+++ sql/createdatabase.sql
@@ -1,30 +1,31 @@
-CREATE TABLE "pb_server" (
+pb_userCREATE TABLE "pb_server" (
     "server_id" SERIAL,
     "server_name" VARCHAR NOT NULL,
-    "server_creationdate" TIMESTAMP,
+    "server_creationdate" TIMESTAMP DEFAULT NOW(),
     PRIMARY KEY (server_id)
 );
 CREATE TABLE "pb_bot" (
     "bot_id" SERIAL,
-    "bot_Name" VARCHAR NOT NULL,
+    "bot_name" VARCHAR NOT NULL,
     "server_id" INTEGER NOT NULL,
-    "bot_creationdate" TIMESTAMP,
+    "bot_creationdate" TIMESTAMP DEFAULT NOW(),
     PRIMARY KEY (bot_id),
     FOREIGN KEY (server_id) REFERENCES pb_server(server_id)
 );
 CREATE TABLE "pb_user" (
     "user_id" SERIAL PRIMARY KEY,
     "user_name" VARCHAR NOT NULL,
-    "user_creationdate" TIMESTAMP
+    "user_discord_id" VARCHAR NULL,
+    "user_creationdate" TIMESTAMP DEFAULT NOW()
 );
 CREATE TABLE "pb_quotes" (
-    "quote_id" SERIAL,
+    "quotes_id" SERIAL,
     "quotes_content" TEXT NOT NULL,
-    "quotes_date" DATE NOT NULL,
+    "quotes_date" CHAR(4) NOT NULL,
     "quotes_author" VARCHAR NULL,
     "user_id" INTEGER NOT NULL, 
     "server_id" INTEGER NOT NULL,
-    "quote_creationdate" TIMESTAMP,
+    "quote_creationdate" TIMESTAMP DEFAULT NOW(),
     PRIMARY KEY (quote_id),
     FOREIGN KEY (server_id) REFERENCES pb_server(server_id),
     FOREIGN KEY (user_id) REFERENCES pb_user(user_id)
@@ -35,7 +36,7 @@ CREATE TABLE "pb_permissions" (
     "user_id" INTEGER NOT NULL,
     "server_id" INTEGER NOT NULL,
     "perm_permission" VARCHAR NOT NULL,
-    "perm_creationdate" TIMESTAMP,
+    "perm_creationdate" TIMESTAMP DEFAULT NOW(),
     PRIMARY KEY (perm_id),
     FOREIGN KEY (server_id) REFERENCES pb_server(server_id),
     FOREIGN KEY (user_id) REFERENCES pb_user(user_id)
@@ -46,7 +47,7 @@ CREATE TABLE "pb_birthday" (
     "bday_name" VARCHAR NOT NULL,
     "server_id" INTEGER NOT NULL,
     "user_id" INTEGER NOT NULL,
-    "bday_creationdate" TIMESTAMP,
+    "bday_creationdate" TIMESTAMP DEFAULT NOW(),
     PRIMARY KEY (bday_id),
     FOREIGN KEY (server_id) REFERENCES pb_server(server_id),
     FOREIGN KEY (user_id) REFERENCES pb_user(user_id)
